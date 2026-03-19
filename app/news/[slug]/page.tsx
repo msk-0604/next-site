e2016b81a3a1c44157ec8efff49b8b61c48184ea
山本真樹 <yamamoto0604@gmail.com>
2026-03-19 15:00:38 +0900

    ニュースをISRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,33 +1,3 @@
-// import { notFound } from "next/navigation";
-// import { getNewsDetail } from "@/app/_libs/microcms";
-// import Article from "@/app/components/Article";
-// import ButtonLink from "@/app/components/ButtonLink";
-// import style from "./page.module.css";
-
-// type Props = {
-//   params: Promise<{
-//     slug: string;
-//   }>;
-//   searchParams: Promise<{
-//     dk?: string;
-//   }>;
-// };
-
-// export default async function Page({ params, searchParams }: Props) {
-//   const { slug } = await params;
-//   const data = await getNewsDetail(slug).catch(notFound);
-
-//   return (
-//     <>
-//       <Article data={data} />
-//       <div className={style.footer}>
-//         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-//       </div>
-//     </>
-//   );
-// }
-
-
 import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/components/Article";
@@ -42,6 +12,8 @@ type Props = {
     dk?: string;
   }>;
 };
+
+export const revalidate = 60;
 
 export default async function Page({ params, searchParams }: Props) {
   const { slug } = await params;
@@ -61,35 +33,4 @@ export default async function Page({ params, searchParams }: Props) {
   );
 }
 
-
-
-// type Props = {
-//   params: Promise<{
-//     slug: string;
-//   }>;
-//   searchParams: Promise<{
-//     dk?: string;
-//   }>;
-//   searchParams: {
-//     dk?: string;
-//   };
-// };
-
-// export default async function Page({ params, searchParams }: Props) {
-//   const { slug } = await params;
-//   const { dk } = await searchParams;
-
-//   const data = await getNewsDetail(slug, {
-//     draftKey: searchParams.dk,
-//   }).catch(notFound);
-
-//   return (
-//     <>
-//       <Article data={data} />
-//       <div className={style.footer}>
-//         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-//       </div>
-//     </>
-//   );
-// }
 
